9efbd7d8fff6ad88fee1409c86ad68086a570430
root <alfaidi.afnan@gmail.com>
2026-01-16 14:19:26 +0000

    random numbers assigned to positive or negative or zaro

--- python-if_else_loops_functions/0-positive_or_negative.py
+++ python-if_else_loops_functions/0-positive_or_negative.py
@@ -3,11 +3,11 @@ import random
 number = random.randint(-10, 10)
 number = int(input('integer of choice: '))
 if number > 0:
-    print('is positive')
+    print(f"{number} is positive")
 
 elif number == 0:
-    print('is zero')
+    print(f"{number} is zero")
 
-else number < 0:
-    print('is negative')
+else:
+    print(f"{number} is negative")
 
