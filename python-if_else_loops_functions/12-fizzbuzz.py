841eeaf0c2e6106fcbfd7568a4f962476311e7a7
root <alfaidi.afnan@gmail.com>
2026-01-17 10:51:21 +0000

    fizzbuzz challenge from 1 to 100

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -9,6 +9,5 @@ def fizzbuzz():
             print("Buzz", end="")
         else:
             print("{}".format(i), end="")
-        
         # Always print a space after every element
         print(" ", end="")
