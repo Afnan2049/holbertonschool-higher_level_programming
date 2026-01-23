b093e0773ac9987c72396c298531c0c116ec14cb
Afnan2049 <alfaidi.afnan@gmail.com>
2026-01-23 15:27:46 +0000

    converting roman numerals to an integer

---
+++ python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+    roman_dict = {
+            'I': 1, 'X': 10, 'C': 100, 'M': 1000, 'V': 5, 'L': 50, 'D': 500
+            }
+    total = 0
+    length = len(roman_string)
+    for i in range(length):
+        current_val = roman_dict.get(roman_string[i], 0)
+        if i + 1 < length:
+            next_val = roman_dict.get(roman_string[i + 1], 0)
+            if current_val < next_val:
+                total -= current_val
+            else:
+                total += current_val
+        else:
+                total += current_val
+                return total
