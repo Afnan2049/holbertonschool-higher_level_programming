09929c70eae68ebc869eaaac55f438fe814adeb0
afnan alfaidi <alfaidi.afnan@gmail.com>
2026-01-28 11:08:09 +0300

    shorter docstring

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 """"
-Demonstrating creating basic class in python
-by creating empty class Square that defines a square.
+Demonstrating creating basic class by creating empty class Square that defines a square.
 """
 
 
 class Square:
    """
    a currently empty class that represents a square
-   will add to it in future tasks
    """
    pass
