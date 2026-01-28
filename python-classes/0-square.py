377d7bd1d7194d8ba11871d40726eeed2df00dfc
afnan alfaidi <alfaidi.afnan@gmail.com>
2026-01-28 10:54:12 +0300

    adding docstring

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+""""
+Demonstrating creating basic class in python
+by creating empty class Square that defines a square.
+"""
+
 class Square:
-    pass
+   """
+   a currently empty class that represents a square
+   """
+   pass
