96fcda26746a616e163c481950950e0d186fd95f
afnan alfaidi <alfaidi.afnan@gmail.com>
2026-01-29 16:31:39 +0300

    Square class defining a square by private instance atribute + instantation

---
+++ python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module defining a square class with size validation"""
+
+
+class Square:
+    """Representing a square with optional size.
+    
+    Args:
+    size (int): size of the square
+
+    Raises:
+    TypeError: If size is not an int.
+    ValueError: if size < 0.
+    """
+    if not instance (size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    self.__size = size
