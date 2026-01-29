c2671a8e2066272e981e6b3b2fb33fe71253526d
afnan alfaidi <alfaidi.afnan@gmail.com>
2026-01-29 16:18:37 +0300

    fixing docstring in file 1-square.py

--- python-classes/1-square.py
+++ python-classes/1-square.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python
-"""
-A module defining a class Square with a private size attribute.
-Demonstrating basic creation and encapsulation in Python
-"""
+"""A module defining a class Square with a private size attribute."""
+
+
 class Square:
-    """
-A class defining a square by its size.
-Attributes:
-__size (any): The size of the square.
-    """
+    """A class defining a square by its size."""
 
     def __int__(self, size):
-        """
-       Initializing the square with a given size.
-       Args:
-       size: The size of the square.
-        """
+        """Initializing square instance with a given size."""
         self.__size = size
