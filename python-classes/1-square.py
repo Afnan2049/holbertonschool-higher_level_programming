415037dee25b2a0f87e1efff19d9690bea31f4a0
afnan alfaidi <alfaidi.afnan@gmail.com>
2026-01-29 16:02:54 +0300

    a class Square that defines a square by private instance attribute and instantation with size

---
+++ python-classes/1-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+"""
+A module defining a class Square with a private size attribute.
+Demonstrating basic creation and encapsulation in Python
+"""
+
+class Square:
+    """
+A class defining a square by its size.
+Attributes:
+__size (any): The size of the square.
+    """
+
+    def __int__(self, size):
+        """
+       Initializing the square with a given size.
+       Args:
+       size: The size of the square.
+        """
+        self.__size = size
