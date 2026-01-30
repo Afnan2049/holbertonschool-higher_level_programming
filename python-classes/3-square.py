e13ac3a2e5695ecddcf56acaf82f4ff21f45386d
afnan alfaidi <alfaidi.afnan@gmail.com>
2026-01-30 08:17:19 +0300

    fixing code in file 3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """This module defines a Square class and computes its area."""
 
-class Square:
-    """Docstring for Square"""
 
-    def __init__(self, size+0):
+class Square:
+    """Represents a square with a private size attribute."""
+
+    def __init__(self, size=0):
         """Initialize a Square with optional size.
 
         Args:
@@ -13,14 +14,14 @@ class Square:
         Raises:
             TypeError: If size is not an integer.
             ValueError: If size is less than 0.
-            """
-        if not instance(size, int):
+        """
+        if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
-            raisw ValueError("size must be >= 0")
+            raise ValueError("size must be >= 0")
 
-            self.__size = size
+        self.__size = size
 
-            def area(self):
-                """Return the current area of the square."""
-                return self.__size ** 2
+    def area(self):
+        """Return the current area of the square."""
+        return self.__size ** 2
