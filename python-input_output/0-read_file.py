ffe73972ea156b426b6639d12b83b85302a2651d
afnan alfaidi <alfaidi.afnan@gmail.com>
2026-02-04 15:56:23 +0300

    fixing? code in file 0-read_file.py

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -11,7 +11,5 @@ def read_file(filename=""):
     Args:
         filename (str): The name of the file to read.
     """
-
-
-with open(filename, mode="r", encoding="utf-8") as f:
-    print(f.read(), end="")
+    with open(filename, mode="r", encoding="utf-8") as f:
+        print(f.read(), end="")
