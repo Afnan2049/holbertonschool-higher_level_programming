cf200836b042751c4cf47eb13dd111a394278a84
afnan alfaidi <alfaidi.afnan@gmail.com>
2026-02-07 20:36:23 +0300

    a class Student that defines a student by: (based on 9-student.py)

---
+++ python-input_output/10-student.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""
+This module defines a Student class with attribute filtering.
+"""
+
+
+class Student:
+    """
+    Defines a student by first_name, last_name, and age.
+    """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes the student instance.
+
+        Args:
+            first_name (str): The first name.
+            last_name (str): The last name.
+            age (int): The age.
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Retrieves a dictionary representation of a Student instance.
+
+        If attrs is a list of strings, only attributes contained
+        in this list are retrieved.
+
+        Args:
+            attrs (list): A list of strings representing attribute names.
+
+        Returns:
+            dict: The filtered or full dictionary of the student.
+        """
+        if isinstance(attrs, list) and all(isinstance(s, str) for s in attrs):
+            res = {}
+            for key in attrs:
+                if key in self.__dict__:
+                    res[key] = self.__dict__[key]
+            return res
+        return self.__dict__
