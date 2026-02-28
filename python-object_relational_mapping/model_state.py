e1c0383ba959aab2b0a5491a226739d7c0db2cf6
root <alfaidi.afnan@gmail.com>
2026-02-28 20:26:59 +0000

    fixing model_state.py file

--- python-object_relational_mapping/model_state.py
+++ python-object_relational_mapping/model_state.py
@@ -1,22 +1,24 @@
 #!/usr/bin/python3
 """
-Contains the class definition of a City.
+Contains the class definition of a State and an instance
+Base = declarative_base().
 """
-from model_state import Base
-from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+
+# Create the Base class for declarative models
+Base = declarative_base()
 
 
-class City(Base):
+class State(Base):
     """
-    City class that links to the MySQL table 'cities'.
+    State class that links to the MySQL table 'states'.
 
     Attributes:
         id (int): Auto-generated, unique integer, primary key.
         name (string): String of max 128 characters, cannot be null.
-        state_id (int): Foreign key to states.id, cannot be null.
     """
-    __tablename__ = 'cities'
+    __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
