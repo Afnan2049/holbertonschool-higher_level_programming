5a2e99f0ee7a7ed24c68873df001ed7fed7d9a73
root <alfaidi.afnan@gmail.com>
2026-02-28 11:12:37 +0000

    fixing file 1-create_user.sql

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,6 +1,7 @@
 -- Creates the MySQL server user user_0d_1 with all privileges
 -- Create user if it doesn't already exist
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+
 -- Grant all privileges on all databases and tables
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
 
